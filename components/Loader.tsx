f6203280c23739773a8648cf19a42bf68a258ec4
Matvei <matvesch@protonmail.com>
2024-05-14 15:21:19 +0300

    replaced tamagui

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,10 +1,11 @@
-import { Spinner } from 'tamagui';
-import { View } from '@tamagui/core';
+import { View } from 'react-native';
+import { TabBarIcon } from '../ui/TabBarIcon';
 
 export default function Loader() {
   return (
     <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-      <Spinner size="large" color="hsl(24, 100%, 75.3%)" />
+      // todo: animate spinner
+      <TabBarIcon name="spinner" color="hsl(24, 100%, 75.3%)" />
     </View>
   );
 }
