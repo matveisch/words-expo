25ce12af7a1d9c0f2225fd4603df1e4e350ec770
Matvei <matvesch@protonmail.com>
2024-06-27 15:53:03 +0300

    moved push to home view

--- views/home/HomeView.tsx
+++ views/home/HomeView.tsx
@@ -2,7 +2,7 @@ import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from '@react-navigation/native-stack';
-import { Alert, View } from 'react-native';
+import { Alert, Platform, View } from 'react-native';
 
 import ListOfDecks from './ListOfDecks';
 import DeckView from './DeckView';
@@ -24,6 +24,10 @@ import { observer } from 'mobx-react';
 import Loader from '../../components/Loader';
 import SubscriptionOffer from '../SubscriptionOffer';
 import { defaultColors } from '../../helpers/colors';
+import { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import Constants from 'expo-constants';
 
 export type RootStackParamList = {
   Decks: undefined;
@@ -37,12 +41,111 @@ export type RootStackParamList = {
   SubscriptionOffer: undefined;
 };
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+async function scheduleDailyNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      sound: 'default',
+      title: 'Learn new words',
+      body: "Don't forget to study today!",
+      data: { someData: 'goes here' },
+    },
+    trigger: {
+      hour: 9,
+      minute: 0,
+      repeats: true,
+    },
+  });
+}
+
+async function checkAndScheduleNotification() {
+  try {
+    const existingNotifications = await Notifications.getAllScheduledNotificationsAsync();
+    const hasScheduledNotification = existingNotifications.some(
+      (notification) =>
+        notification.content.title === 'Learn new words' &&
+        notification.content.body === "Don't forget to study today!"
+    );
+
+    if (!hasScheduledNotification) {
+      await scheduleDailyNotification();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+function handleRegistrationError(errorMessage: string) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (!Device.isDevice) {
+    handleRegistrationError('Must use physical device for push notifications');
+    return;
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+    handleRegistrationError('Permission not granted to get push token for push notification!');
+    return;
+  }
+
+  const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+  if (!projectId) {
+    handleRegistrationError('Project ID not found');
+    return;
+  }
+
+  try {
+    const pushTokenData = await Notifications.getExpoPushTokenAsync({
+      projectId,
+    });
+    return pushTokenData.data;
+  } catch (e: unknown) {
+    handleRegistrationError(`${e}`);
+  }
+}
+
 const HomeView = observer(() => {
   const { mutateAsync: deleteDeck, isPending: deckIsBeingDeleted } = useDeleteDeck();
   const { mutateAsync: deleteWord, isPending: wordIsBeingDeleted } = useDeleteWord();
   const { data: decks } = useDecks(sessionStore.session?.user.id || '');
   const { data: user } = useUser(sessionStore.session?.user.id || '');
   const Stack = createNativeStackNavigator<RootStackParamList>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => {
+        if (token) {
+          checkAndScheduleNotification();
+        }
+      })
+      .catch((error: any) => console.error(error));
+  }, []);
 
   function handleDeleteDeck(
     deckId: number,
