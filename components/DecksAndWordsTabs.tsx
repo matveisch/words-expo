840b93212a7c3710e8492cd6e8421fcffc50ffcc
Matvei <matvesch@protonmail.com>
2024-07-05 09:45:34 +0300

    words tab

--- components/DecksAndWordsTabs.tsx
+++ components/DecksAndWordsTabs.tsx
@@ -1,4 +1,3 @@
-import { FlashList } from '@shopify/flash-list';
 import { useEffect, useRef, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -8,22 +7,15 @@ import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-na
 import { observer } from 'mobx-react-lite';
 import { useIsMutating } from '@tanstack/react-query';
 
-import { useWords } from '../hooks/useWords';
 import { RootStackParamList } from '../views/home/HomeView';
 import Button from '../ui/Button';
-import ListItem from '../ui/ListItem';
 import { defaultColors } from '../helpers/colors';
 import ThemedText from '../ui/ThemedText';
-import { useDecks } from '../hooks/useDecks';
 import { sessionStore } from '../features/sessionStore';
 import useUser from '../hooks/useUser';
 import Loader from './Loader';
-import LockedFeature from './LockedFeature';
-import ListItemSkeleton from '../ui/ListItemSkeleton';
-import Input from '../ui/Input';
-import Stats from './Stats';
-import StudyButtons from './StudyButtons';
 import DecksTab from './DecksTab';
+import WordsTab from './WordsTab';
 
 type Props = {
   currentDeck: number;
@@ -44,14 +36,6 @@ const DecksAndWordsTabs = observer((props: Props) => {
   const { currentDeck, hasParentDeck } = props;
   const [activeTab, setActiveTab] = useState(0);
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
-
-  const { data: decks, isFetched } = useDecks(sessionStore.session?.user.id || '');
-  const subDecks = decks?.filter((d) => d.parent_deck === currentDeck);
-
-  const decksIds = [...(subDecks?.map((deck) => deck.id) || []), currentDeck];
-  const { data, fetchNextPage, hasNextPage } = useWords(currentDeck, decksIds, isFetched);
-  // Combine all pages into a single array
-  const words = data?.pages.flat() || [];
 
   const { data: user } = useUser(sessionStore.session?.user.id || '');
 
@@ -141,58 +125,7 @@ const DecksAndWordsTabs = observer((props: Props) => {
         scrollEnabled={!hasParentDeck}
         onPageSelected={(e) => setActiveTab(e.nativeEvent.position)}
       >
-        <View style={{ width: '100%', height: '100%' }} key="1">
-          {words && (
-            <FlashList
-              onEndReached={() => {
-                if (hasNextPage && user.pro) fetchNextPage();
-              }}
-              onEndReachedThreshold={0.5}
-              showsVerticalScrollIndicator={false}
-              estimatedItemSize={65}
-              data={user?.pro ? words : words.slice(0, 20)}
-              ListEmptyComponent={<Text style={{ textAlign: 'center' }}>No words</Text>}
-              ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-              renderItem={({ item }) => (
-                <ListItem
-                  title={item.word}
-                  subTitle={item.meaning}
-                  onPress={() => {
-                    navigation.push('Word', {
-                      word: item,
-                    });
-                  }}
-                />
-              )}
-              ListHeaderComponent={
-                <View style={{ marginBottom: 10 }}>
-                  <Stats deckId={currentDeck} />
-                  <StudyButtons deckId={currentDeck} />
-                  <Input placeholder="Search" />
-                </View>
-              }
-              ListFooterComponent={
-                !user?.pro && words?.length && words.length > 19 ? (
-                  <View style={{ marginTop: 10 }}>
-                    <LockedFeature text="Get pro version to view and create more than 20 words" />
-                  </View>
-                ) : (
-                  <View style={{ height: 80 }} />
-                )
-              }
-            />
-          )}
-          {!words && (
-            <FlashList
-              estimatedItemSize={65}
-              showsVerticalScrollIndicator={false}
-              renderItem={() => <ListItemSkeleton height={65} />}
-              data={[...Array(3)]}
-              ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
-            />
-          )}
-        </View>
-
+        <WordsTab deckId={currentDeck} />
         <DecksTab deckId={currentDeck} />
       </PagerView>
 
---
+++ components/WordsTab.tsx
@@ -0,0 +1,140 @@
+import { FlashList } from '@shopify/flash-list';
+import { Text, View, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../views/home/HomeView';
+import { observer } from 'mobx-react-lite';
+import { useMemo } from 'react';
+
+import ListItem from '../ui/ListItem';
+import Stats from './Stats';
+import StudyButtons from './StudyButtons';
+import LockedFeature from './LockedFeature';
+import ListItemSkeleton from '../ui/ListItemSkeleton';
+import { useDecks } from '../hooks/useDecks';
+import { sessionStore } from '../features/sessionStore';
+import { useWords } from '../hooks/useWords';
+import useUser from '../hooks/useUser';
+
+type Props = {
+  deckId: number;
+};
+
+const WordsTab = observer(({ deckId }: Props) => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const {
+    data: user,
+    isLoading: userLoading,
+    error: userError,
+  } = useUser(sessionStore.session?.user.id || '');
+
+  const {
+    data: decks,
+    isFetched: decksFetched,
+    isLoading: decksLoading,
+    error: decksError,
+  } = useDecks(sessionStore.session?.user.id || '');
+
+  const subDecks = useMemo(() => decks?.filter((d) => d.parent_deck === deckId), [decks, deckId]);
+  const decksIds = useMemo(
+    () => [...(subDecks?.map((deck) => deck.id) || []), deckId],
+    [subDecks, deckId]
+  );
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading: wordsLoading,
+    error: wordsError,
+  } = useWords(deckId, decksIds, decksFetched);
+  // Combine all pages into a single array
+  const words = useMemo(() => data?.pages.flat() || [], [data]);
+
+  if (userLoading || decksLoading || wordsLoading) {
+    return (
+      <FlashList
+        estimatedItemSize={65}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => <ListItemSkeleton height={65} />}
+        data={[...Array(3)]}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
+    );
+  } else if (userError || decksError || wordsError) {
+    return <Text style={styles.errorText}>Failed to load data</Text>;
+  }
+
+  return (
+    <View style={styles.container} key="1">
+      <FlashList
+        onEndReached={() => {
+          if (hasNextPage && user?.pro) fetchNextPage();
+        }}
+        onEndReachedThreshold={0.5}
+        showsVerticalScrollIndicator={false}
+        estimatedItemSize={65}
+        data={user?.pro ? words : words.slice(0, 20)}
+        ListEmptyComponent={<Text style={styles.emptyText}>No words</Text>}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.word}
+            subTitle={item.meaning}
+            onPress={() => {
+              navigation.push('Word', {
+                word: item,
+              });
+            }}
+          />
+        )}
+        ListHeaderComponent={
+          <View>
+            <Stats deckId={deckId} />
+            <StudyButtons deckId={deckId} />
+            {/*<Input placeholder="Search" />*/}
+          </View>
+        }
+        ListFooterComponent={
+          !user?.pro && words?.length && words.length > 19 ? (
+            <View style={styles.lockedFeature}>
+              <LockedFeature text="Get pro version to view and create more than 20 words" />
+            </View>
+          ) : (
+            <View style={styles.footer} />
+          )
+        }
+      />
+    </View>
+  );
+});
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  separator: {
+    height: 10,
+  },
+  footer: {
+    height: 80,
+  },
+  lockedFeature: {
+    marginTop: 10,
+  },
+  emptyText: {
+    textAlign: 'center',
+  },
+  errorText: {
+    textAlign: 'center',
+    color: 'red',
+  },
+});
+
+export default WordsTab;
