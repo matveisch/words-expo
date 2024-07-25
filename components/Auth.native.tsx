0fbd679fbed59dbcb8cd4f922c0f81c6c2a52b83
Matvei <matvesch@protonmail.com>
2024-07-25 10:48:18 +0300

    updates, removed google sign in from ios

--- components/Auth.native.tsx
+++ components/Auth.native.tsx
@@ -6,7 +6,7 @@ export const Auth = () => {
   return (
     <>
       {Platform.OS === 'ios' && <AppleButton />}
-      <GoogleButton />
+      {Platform.OS === 'android' && <GoogleButton />}
     </>
   );
 };
