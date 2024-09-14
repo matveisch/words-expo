3f98f046a9bc34ef25528d4534fc0ad7e275737e
Matvei <matvesch@protonmail.com>
2024-09-14 15:31:38 +0300

    fixes

--- views/settings/Settings.tsx
+++ views/settings/Settings.tsx
@@ -1,24 +1,25 @@
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import { Keyboard, View, Text, Switch, StyleSheet } from 'react-native';
-import Toast from 'react-native-root-toast';
 import { observer } from 'mobx-react-lite';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { Keyboard, StyleSheet, Switch, View } from 'react-native';
+import Toast from 'react-native-root-toast';
 
+import RNDateTimePicker from '@react-native-community/datetimepicker';
+import Loader from '../../components/Loader';
+import LockedFeature from '../../components/LockedFeature';
+import SpecialCodeInput from '../../components/SpecialCodeInput';
 import User from '../../components/User';
-import { TabBarIcon } from '../../ui/TabBarIcon';
-import Label from '../../ui/Label';
-import Input from '../../ui/Input';
-import Description from '../../ui/Description';
-import Button from '../../ui/Button';
+import { autoCheckStore } from '../../features/autoCheckStore';
+import { pushStore } from '../../features/pushStore';
+import { sessionStore } from '../../features/sessionStore';
 import { wordsLimitStore } from '../../features/wordsLimitStore';
 import { toastOptions } from '../../helpers/toastOptions';
-import { autoCheckStore } from '../../features/autoCheckStore';
 import useUser from '../../hooks/useUser';
-import { sessionStore } from '../../features/sessionStore';
-import LockedFeature from '../../components/LockedFeature';
-import Loader from '../../components/Loader';
-import RNDateTimePicker from '@react-native-community/datetimepicker';
-import { pushStore } from '../../features/pushStore';
-import SpecialCodeInput from '../../components/SpecialCodeInput';
+import Button from '../../ui/Button';
+import Description from '../../ui/Description';
+import Input from '../../ui/Input';
+import InputError from '../../ui/InputError';
+import Label from '../../ui/Label';
+import { TabBarIcon } from '../../ui/TabBarIcon';
 
 type Inputs = {
   wordsPerSet: string;
@@ -82,7 +83,7 @@ const Settings = observer(() => {
                   <TabBarIcon name="check" size={20} />
                 </Button>
               </View>
-              {errors.wordsPerSet && <Text style={{ color: 'red' }}>This field is required</Text>}
+              {errors.wordsPerSet && <InputError text="This field is required" />}
             </View>
 
             <View style={[styles.row, { marginTop: 20 }]}>
