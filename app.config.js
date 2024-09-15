459daf13dd871d2390ce3881688e167a9fb3ad2f
Matvei <matvesch@protonmail.com>
2024-09-15 08:44:02 +0300

    version increase

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: 'wordem',
     slug: 'wordem',
-    version: '1.1.1',
+    version: '1.2.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
